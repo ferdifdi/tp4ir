752a50944e61d77362a93d2d29129617ab4c8a9e
ferdifdi <ferdifadillah3@gmail.com>
2022-12-11 22:49:22 +0700

    [FIX] fix minor lg

--- home/bsbi.py
+++ home/bsbi.py
@@ -45,17 +45,17 @@ class BSBIIndex:
     def save(self):
         """Menyimpan doc_id_map and term_id_map ke output directory via pickle"""
 
-        with open(os.path.join(self.output_dir, os.path.dirname(__file__) +'\\index\\terms.dict'), 'wb') as f:
+        with open(os.path.join(self.output_dir+os.path.dirname(__file__)+  '/index/terms.dict'), 'wb') as f:
             pickle.dump(self.term_id_map, f)
-        with open(os.path.join(self.output_dir, os.path.dirname(__file__) +'\\index\\docs.dict'), 'wb') as f:
+        with open(os.path.join(self.output_dir+os.path.dirname(__file__)+ '/index/docs.dict'), 'wb') as f:
             pickle.dump(self.doc_id_map, f)
 
     def load(self):
         """Memuat doc_id_map and term_id_map dari output directory"""
 
-        with open(os.path.join(self.output_dir, os.path.dirname(__file__) +'\\index\\terms.dict'), 'rb') as f:
+        with open(os.path.join(self.output_dir, os.path.dirname(__file__)+ '/index/terms.dict'), 'rb') as f:
             self.term_id_map = pickle.load(f)
-        with open(os.path.join(self.output_dir, os.path.dirname(__file__) +'\\index\\docs.dict'), 'rb') as f:
+        with open(os.path.join(self.output_dir, os.path.dirname(__file__)+ '/index/docs.dict'), 'rb') as f:
             self.doc_id_map = pickle.load(f)
         with InvertedIndexReader(
                 directory=self.output_dir,
